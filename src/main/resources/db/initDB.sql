94431a28e05951a0fc2ad92c49c5db87db2fb4e5
Vladi207 <graschenko207@mail.ru>
2022-02-26 13:51:11 +0300

    fix DBinit

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS meals;
+DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
@@ -31,7 +31,7 @@ CREATE TABLE meals
     user_id     INTEGER   NOT NULL,
     date_time   TIMESTAMP NOT NULL,
     description TEXT      NOT NULL,
-    calories    INTEGER   NOT NULL,
+    calories    INT   NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 CREATE UNIQUE INDEX meals_unique_user_datetime_idx ON meals(user_id, date_time);
