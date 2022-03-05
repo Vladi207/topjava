66b02249e644987c00ba5d12a8fb3daeb621a8f4
Vladi207 <graschenko207@mail.ru>
2022-03-05 16:54:57 +0300

    HW05

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudUserRepository.java
@@ -1,10 +1,12 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 
 @Transactional(readOnly = true)
